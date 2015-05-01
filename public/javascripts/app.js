71478b90b483dd55b6451546b8ed3710816a35a0
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-01 22:20:42 +0000

    Added an overlay to the collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -194,7 +194,9 @@ require("./album");
 var buildAlbumThumbnail = function() {
    var template =
        '<div class="collection-album-container col-md-2">'
-     + '  <img src="/images/album-placeholder.png"/>'
+     + '  <div class="collection-album-image-container">'
+     + '    <img src="/images/album-placeholder.png"/>'
+     + '  </div>'
      + '  <div class="caption album-collection-info">'
      + '    <p>'
      + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -222,6 +224,16 @@ var updateCollectionView = function() {
         var $newThumbnail = buildAlbumThumbnail();
         $collection.append($newThumbnail);
     }
+
+    var onHover = function(event) {
+        $(this).append(buildAlbumOverlay("/album.html"));
+    };
+
+    var offHover = function(event) {
+        $(this).find('.collection-album-image-overlay').remove();
+    };
+
+    $collection.find('.collection-album-image-container').hover(onHover, offHover);
 };
 
 if (document.URL.match(/\/collection.html/)) {
@@ -229,6 +241,23 @@ if (document.URL.match(/\/collection.html/)) {
     $(document).ready(function() {
         updateCollectionView();
     });
+};
+
+var buildAlbumOverlay = function(albumURL) {
+  var template =
+      '<div class="collection-album-image-overlay">'
+    + '  <div class="collection-overlay-content">'
+    + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+    + '      <i class="fa fa-play"></i>'
+    + '    </a>'
+    + '    &nbsp;'
+    + '    <a class="collection-overlay-button">'
+    + '      <i class="fa fa-plus"></i>'
+    + '    </a>'
+    + '  </div>'
+    + '</div>'
+    ;
+  return $(template);
 };
 
 });
