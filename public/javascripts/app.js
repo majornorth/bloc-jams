befa8151eb2a312a00d9d1a14f916ba0f2bfe7f1
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-01 20:47:10 +0000

    Used jQuery to populate the collection view.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -92,6 +92,50 @@
 })();
 require.register("scripts/app", function(exports, require, module) {
 require("./landing");
+require("./collection");
+
+});
+
+;require.register("scripts/collection", function(exports, require, module) {
+var buildAlbumThumbnail = function() {
+   var template =
+       '<div class="collection-album-container col-md-2">'
+     + '  <img src="/images/album-placeholder.png"/>'
+     + '  <div class="caption album-collection-info">'
+     + '    <p>'
+     + '      <a class="album-name" href="/album.html"> Album Name </a>'
+     + '      <br/>'
+     + '      <a href="/album.html"> Artist name </a>'
+     + '      <br/>'
+     + '      X songs'
+     + '      <br/>'
+     + '      X:XX Total Length'
+     + '      <br/>'
+     + '    </p>'
+     + '  </div>'
+     + '</div>';
+
+  return $(template);
+};
+
+var updateCollectionView = function() {
+    var $collection = $(".collection-container .row");
+    $collection.empty();
+
+    var randomNum = Math.floor((Math.random() * 100) + 1);
+
+    for (var i = 0; i < randomNum; i++) {
+        var $newThumbnail = buildAlbumThumbnail();
+        $collection.append($newThumbnail);
+    }
+};
+
+if (document.URL.match(/\/collection.html/)) {
+    // Wait until the HTML is fully processed.
+    $(document).ready(function() {
+        updateCollectionView();
+    });
+};
 
 });
 
