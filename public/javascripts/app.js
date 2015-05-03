8340b132a8b65dcfec7bed596359f7fe72c146b7
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-03 21:30:33 +0000

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -270,10 +270,40 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+// require("./landing");
+// require("./collection");
+// require("./album");
+// require("./profile");
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+    $scope.headerText = "Bloc Jams";
+    $scope.subText = "Turn the music up!";
+
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+    $scope.subTextClicked = function() {
+        $scope.subText += '!';
+    };
+
+    $scope.albumURLs = [
+        '/images/album-placeholders/album-1.jpg',
+        '/images/album-placeholders/album-2.jpg',
+        '/images/album-placeholders/album-3.jpg',
+        '/images/album-placeholders/album-4.jpg',
+        '/images/album-placeholders/album-5.jpg',
+        '/images/album-placeholders/album-6.jpg',
+        '/images/album-placeholders/album-7.jpg',
+        '/images/album-placeholders/album-8.jpg',
+        '/images/album-placeholders/album-9.jpg',
+    ];
+
+    $scope.headerTextClicked = function() {
+        return shuffle($scope.albumURLs);
+    };
+}]);
 
 });
 
