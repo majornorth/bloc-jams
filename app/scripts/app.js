ac417eef728144ac4478cd6a2822d00be6bb79da
Stewart McCoy <mccoy.stewart@gmail.com>
2015-05-04 22:49:37 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,6 +38,11 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
             url: '/collection',
             controller: 'Collection.controller',
             templateUrl: '/templates/collection.html'
+        })
+        .state('album', {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '/templates/album.html'
         });
 }]);
 
@@ -77,3 +82,36 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
         $scope.albums.push(angular.copy(albumPicasso));
     }
 }])
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+  var playingSong = null;
+
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
+
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
+
+  $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+
+   $scope.playSong = function(song) {
+    playingSong = song;
+   };
+
+   $scope.pauseSong = function(song) {
+     playingSong = null;
+   };
+}]);
